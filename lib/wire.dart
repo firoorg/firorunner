c7469ec55828cec8f3912529b7ca185223e98599
marco <marco@cypherstack.com>
2021-09-13 15:00:43 -0600

    Made the wires collision box smaller.

--- lib/wire.dart
+++ lib/wire.dart
@@ -1,6 +1,7 @@
 import 'package:firo_runner/moving_object.dart';
 import 'package:firo_runner/main.dart';
 import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
 
 enum WireState { normal }
 
@@ -37,5 +38,35 @@ class Wire extends MovingObject {
 
   void remove() {
     sprite.remove();
+  }
+
+  @override
+  String intersect(Rect other) {
+    Rect currentRect = sprite.toRect();
+    Rect wireRect = Rect.fromLTWH(
+      currentRect.left + 2 * currentRect.width / 5,
+      currentRect.top + 2 * currentRect.height / 7,
+      currentRect.width / 5,
+      currentRect.height / 5,
+    );
+    final collision = wireRect.intersect(other);
+    if (!collision.isEmpty) {
+      double yDistance = other.top - wireRect.top;
+      double xDistance = other.left - wireRect.left;
+      if (yDistance.abs() > xDistance.abs()) {
+        if (yDistance > 0) {
+          return "bottom";
+        } else {
+          return "top";
+        }
+      } else {
+        if (xDistance > 0) {
+          return "right";
+        } else {
+          return "left";
+        }
+      }
+    }
+    return "none";
   }
 }
